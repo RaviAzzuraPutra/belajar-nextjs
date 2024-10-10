4ffc1c7f2ee976c683625988ae5ce3ffdfc93e01
RaviAzzuraPutra <ravi.azzura74@gmail.com>
2024-10-10 14:33:10 +0700

    title metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog"
+    },
+    description: 'My Blog',
+}
+
 export default function Blog() {
     return (
         <h1>My Blog</h1>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "My Profile",
+    description: "My Profile"
+}
+
 export default function Profile() {
     return (
         <h1>My Profile</h1>
